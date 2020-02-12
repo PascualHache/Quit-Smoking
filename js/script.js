0005c4980c19d89c5848ad29904b42a86ae0a306
PascualHache <hector.pascual.diaz@gmail.com>
2020-02-12 11:01:28 +0100

    Acumulacion implementada

--- js/script.js
+++ js/script.js
@@ -1,42 +1,88 @@
 window.onload=iniciar;
+
+var clickedByUser = false;
+
 function iniciar(){
-  document.getElementById("icon1").addEventListener("mouseover", mouseOver);
-  document.getElementById("icon1").addEventListener("mouseout", mouseOut);
-  document.getElementById("icon2").addEventListener("mouseover", mouseOver);
-  document.getElementById("icon2").addEventListener("mouseout", mouseOut);
-  document.getElementById("icon3").addEventListener("mouseover", mouseOver);
-  document.getElementById("icon3").addEventListener("mouseout", mouseOut);
-  document.getElementById("icon4").addEventListener("mouseover", mouseOver);
-  document.getElementById("icon4").addEventListener("mouseout", mouseOut);
-  document.getElementById("icon5").addEventListener("mouseover", mouseOver);
-  document.getElementById("icon5").addEventListener("mouseout", mouseOut);
+  for (var i = 1; i < 6; i++) {
+    document.getElementById("icon"+(i)).addEventListener("mouseover", mouseOver);
+    document.getElementById("icon"+(i)).addEventListener("mouseout", mouseOut);
+    document.getElementById("icon"+(i)).addEventListener("click", clickOnIcon);
+  }
 }
 
 function mouseOver() {
-  var currentPos = getPosition(this.id);
-  console.log(currentPos);
-  getImgList(currentPos,"#c00");
+    // getImgList(getPosition(this.id),"#c00");
+    console.log("mouseOver");
+    paintArr(getImgList(this.id),"orange");
 }
 
 function mouseOut() {
-  getImgList(getPosition(this.id),"orange");
+    // getImgList(getPosition(this.id),"orange");
+    paintArr(getImgList(this.id),"#f7d674");
 }
 
 function getPosition(nameID){
   return nameID.charAt(4);
 }
 
-function getImgList(currentPos, cColor){
+function getImgList(currentPos){
   imgArr = []
-  for (var i = 0; i < currentPos; i++) {
+  console.log(currentPos);
+  if (clickedByUser == false){
+  for (var i = 0; i < getPosition(currentPos); i++) {
     imgArr.push("icon"+(i+1));
-    // console.log(imgArr);
-    paintArr(imgArr, cColor);
+    // paintArr(imgArr, cColor);
   }
+}
+ return imgArr;
 }
 
 function paintArr(aImages, cColor){
+  console.log(aImages);
   for (var i = 0; i < aImages.length; i++) {
     document.getElementById(aImages[i]).style.fill = cColor;
   }
 }
+
+function clickOnIcon(){
+  clickedByUser = true;
+  if(clickedByUser){
+    paintArr(getImgList(this.id),"green");
+} else{
+  paintArr(getImgList("icon5"),"#f7d674");
+}
+  console.log(this.id);
+}
+
+function clickOffIcon(){
+  clickedByUser = false;
+  paintArr(getImgList("icon5"),"#f7d674");
+}
+
+document.addEventListener("click", (evt) => {
+    const flyoutElement1 = document.getElementById("icon1");
+    const flyoutElement2 = document.getElementById("icon2");
+    const flyoutElement3 = document.getElementById("icon3");
+    const flyoutElement4 = document.getElementById("icon4");
+    const flyoutElement5 = document.getElementById("icon5");
+    let targetElement = evt.target; // clicked element
+
+    do {
+        if (targetElement == flyoutElement1 || targetElement == flyoutElement2 || targetElement == flyoutElement3 || targetElement == flyoutElement4 || targetElement == flyoutElement5) {
+            // This is a click inside. Do nothing, just return.
+            // document.getElementById("icon1").textContent = "Clicked inside!";
+            console.log("Clicked inside!");
+            clickedByUser = false;
+            clickOnIcon();
+            return;
+        }
+        // Go up the DOM
+        targetElement = targetElement.parentNode;
+    } while (targetElement);
+
+    // This is a click outside.
+    // document.getElementById("icon1").textContent = "Clicked outside!";
+    console.log("Clicked OUTTTTTTT!");
+    clickedByUser = true;
+    clickOffIcon();
+});
